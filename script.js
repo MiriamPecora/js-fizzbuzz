5d733e0fee9908193f03c82db2cced1969fe0d9e
MiriamPecora <miriampecora97@gmail.com>
2024-05-01 19:10:48 +0200

    [COMPLETED] Completed task with first and second bonus; added css classes using javascript.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Dichiaro le variabili contenenti le stringhe che utilizzerò
-const fizzBuzz = "FizzBuzz";
-const fizz = "Fizz";
-const buzz = "Buzz";
+const fizzBuzz = "fizzbuzz";
+const fizz = "fizz";
+const buzz = "buzz";
 
 // Inizio il ciclo for
 for (let isDivisible = 1; isDivisible <= 100; isDivisible++ ) {
@@ -9,6 +9,7 @@ for (let isDivisible = 1; isDivisible <= 100; isDivisible++ ) {
     // Dichiaro le variabili che mi serviranno per creare nuovi elementi nel DOM
     const numberList = document.getElementById("number_list");
     const liElement = document.createElement('li');
+    liElement.classList.add('square');
 
     // Faccio svolgere le operazioni condizionali per determinare quali numeri sono divisibili per 3, 5, 15 o nessuno dei tre
     if (isDivisible % 3 !== 0 && isDivisible % 5 !== 0)
@@ -23,18 +24,22 @@ for (let isDivisible = 1; isDivisible <= 100; isDivisible++ ) {
     else if (isDivisible % 15 == 0)
     {
         liElement.innerHTML = fizzBuzz;
+        // Aggiungo una classe a quella già presente
+        liElement.classList.add('pink-bg');
         numberList.append(liElement)
         console.log(fizzBuzz)
     }
     else if (isDivisible % 3 === 0)
     {
         liElement.innerHTML = fizz;
+        liElement.classList.add('green-bg');
         numberList.append(liElement)
         console.log(fizz)
     }
     else if (isDivisible % 5 === 0)
     {
         liElement.innerHTML = buzz;
+        liElement.classList.add('gold-bg');
         numberList.append(liElement)
         console.log(buzz)
     }
